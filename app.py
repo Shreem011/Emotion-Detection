487878f89ada3a694bc111bda2e74f8d14807c2e
Shreem <shreemaryal@gmail.com>
2022-02-27 16:39:44 +0545

    simple frontend done

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 
-from scipy.misc import imread, imresize
+
 from imageio import imwrite
+import imageio
 import numpy as np
 import keras.models
 import re
 import sys
 import os
+import cv2
 
 # Dependencies
 import base64
@@ -18,15 +20,24 @@ from keras.models import Sequential
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+
+imread = imageio.imread
 
 
 # Initialize the app
-app = Flask(__name__)
+
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, './static/')
+
+app = Flask(__name__, template_folder=template_path)
 
 def get_model():
-	global model
-	model = load_model('./model/cifar10_ResNet20v1_model.h5')
-	model._make_predict_function() # It gives TensorFlow error if this is omitted.
+	global emotion_model
+	emotion_model = load_model('./model/model.h5')
+	# model._make_predict_function() # It gives TensorFlow error if this is omitted.
 	print('Model Loaded!')
 
 def preprocess_image(image, target_size):
@@ -41,56 +52,73 @@ def preprocess_image(image, target_size):
 	c[0] = img
 	return c
 
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    
+    plt.bar(y_pos, emotions, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('percentage')
+    plt.title('emotion')
+    
+    plt.show()
+	
 print('Loading model..............') 
 get_model()
 
 # Prediction route
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST','GET'])
+
 def predict():
-	message = request.get_json(force=True)
-	encoded = message['image']
-	decoded = base64.b64decode(encoded)
-	image = Image.open(io.BytesIO(decoded))
+	# message = request.json(force=True)
+	# print(message)
+	# encoded = message['image']
+	# decoded = base64.b64decode(encoded)
+	# image = Image.open(io.BytesIO(decoded))
+	# print(image)
 
-	# Enter the dimension of the image accepted ny your model
-	processed_image = preprocess_image(image, target_size=(32,32)) 
+	f = request.files['file']
+	f.save(f.filename)
+	file = "./"+f.filename
 
-	prediction = model.predict(processed_image)
+	# file = request.get_json(force=True)
 
-	cifar10_labels = np.array([
-    'airplane',
-    'automobile',
-    'bird',
-    'cat',
-    'deer',
-    'dog',
-    'frog',
-    'horse',
-    'ship',
-    'truck'])
+	true_image = image.load_img(file)
+	img = image.load_img(file, color_mode="grayscale", target_size=(48, 48))
+	print("aaaa")
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis = 0)
 
-	bestnum = 0.0
-	bestclass = 0
-	for n in [0,1,2,3,4,5,6,7,8,9]:
-		if bestnum < prediction[0][n]:
-			bestnum = prediction[0][n]
-			bestclass = n
-	
-	predicted_class = cifar10_labels[bestclass]
+	x /= 255
 
-	# print(prediction)
-	# print(predicted_class)
+	custom = emotion_model.predict(x)
+	print(custom[0])
+	emotion_analysis(custom[0])
+
+	x = np.array(x, 'float32')
+	x = x.reshape([48, 48])
+
+
+	plt.imshow(true_image)
+	plt.show()
+
+	result = np.where(custom[0]==max(custom[0]))
+	print(result[0][0])
+
+	objects = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+	emotion = objects[result[0][0]]
 
 	response = {
-		'predicted_class' : predicted_class
+		'predicted_class' : emotion
 	}
+	
 
 	return jsonify(response)
 
 # Default app route
 @app.route('/')
 def hello():
-	return "Hello, world!"
+	return render_template('predict.html')
 
 @app.route('/hello', methods = ['POST'])
 def hi():
